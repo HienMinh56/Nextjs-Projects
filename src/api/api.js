d786a822e8fb2131839a0136c245bd1a78397966
Leo0Nguyen <linhnguyen41103@gmail.com>
2023-11-10 00:35:22 +0700

    fix UI admin

--- src/api/api.js
+++ src/api/api.js
@@ -58,6 +58,14 @@ export const getCourseDetails = async (course_id) => {
   return res.data;
 };
 
+export const postCourseDetails = async (data, token) => {
+  const res = await axios.post(
+    `http://localhost:8080/coursedetail/${course_id}`
+  );
+  console.log(res);
+  return res.data;
+};
+
 export const getCourseById = async (id) => {
   {
     const res = await axios.get(`http://localhost:8080/courses/${id}`);
@@ -105,8 +113,24 @@ export const orderCourse = async (data) => {
   return res.data;
 };
 
+export const getOrderCourse = async (token) => {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+  const res = await axios.get(`http://localhost:8080/ordercourses`);
+  console.log(res);
+  return res.data;
+};
+
 export const orderTool = async (data) => {
   const res = await axios.post(`http://localhost:8080/orderitems`, data);
+  console.log(res);
+  return res.data;
+};
+
+export const getOrderTool = async (token) => {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+  const res = await axios.get(`http://localhost:8080/orderitems`);
   console.log(res);
   return res.data;
 };
@@ -114,6 +138,16 @@ export const orderTool = async (data) => {
 export const getCourseByCategory = async (id) => {
   {
     const res = await axios.get(`http://localhost:8080/courses/category/${id}`);
+    console.log(res);
+    return res.data;
+  }
+};
+
+export const getCourseByInstructor = async (username) => {
+  {
+    const res = await axios.get(
+      `http://localhost:8080/courses/instructor/${username}`
+    );
     console.log(res);
     return res.data;
   }
@@ -133,10 +167,27 @@ export const postProfileImage = async (formData, username) => {
   return res.data;
 };
 
+export const postCourseImage = async (formData, course_id) => {
+  const res = await axios.post(
+    `http://localhost:8080/courses/upload/image/${course_id}`,
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data", // Set the content type for a file upload
+      },
+    }
+  );
+  console.log(res);
+  return res.data;
+};
+
 export const changePassword = async (data, token) => {
   axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-  const res = await axios.post(`http://localhost:8080/users/change-password`, data);
+  const res = await axios.post(
+    `http://localhost:8080/users/change-password`,
+    data
+  );
   console.log(res);
   return res.data;
 };
@@ -153,34 +204,139 @@ export const getUser = async (username) => {
   return res.data;
 };
 
+export const getAllUser = async (token) => {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+  const res = await axios.get(`http://localhost:8080/users`);
+  console.log(res);
+  return res.data;
+};
+
+export const getAllInstructor = async () => {
+  const res = await axios.get(`http://localhost:8080/users/instructor`);
+  console.log(res);
+  return res.data;
+};
+
+
 export const getCourseCategory = async () => {
   const res = await axios.get(`http://localhost:8080/coursecategory`);
   console.log(res);
   return res.data;
 };
 
-export const searchCourse = async (title) => {
-  const res = await axios.get(`http://localhost:8080/courses/search?title=${title}`);
+export const getTooleCategory = async () => {
+  const res = await axios.get(`http://localhost:8080/itemcategory`);
   console.log(res);
   return res.data;
 };
+
+export const searchCourse = async (title) => {
+  const res = await axios.get(
+    `http://localhost:8080/courses/search?title=${title}`
+  );
+  console.log(res);
+  return res.data;
+};
+
+export const searchTool = async (name) => {
+  const res = await axios.get(
+    `http://localhost:8080/items/search?name=${name}`
+  );
+  console.log(res);
+  return res.data;
+};
+
+export const delCourse = async (id, token) => {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+  const res = await axios
+    .delete(`http://localhost:8080/courses/${id}`)
+    .catch((err) => toast(err.message, { type: toast.TYPE.ERROR }));
+  console.log(res);
+  return res.data;
+};
+
+export const postCourse = async (data, token) => {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+  const res = await axios
+    .post(`http://localhost:8080/courses`, data)
+    .catch((err) => toast(err.message, { type: toast.TYPE.ERROR }));
+  console.log(res);
+  return res.data;
+};
+
+export const editCourse = async (data, token) => {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+  const res = await axios
+    .put(`http://localhost:8080/courses`, data)
+    .catch((err) => toast(err.message, { type: toast.TYPE.ERROR }));
+  console.log(res);
+  return res.data;
+};
+
+export const editCourseDetail = async (data, token) => {
+  console.log("data", data);
+  console.log("token", token);
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+
+  const res = await axios
+    .post(`http://localhost:8080/coursedetail/list`, data)
+    .catch((err) => toast(err.message, { type: toast.TYPE.ERROR }));
+  console.log(res);
+  return res.data;
+};
+
+export const getOrderHistory = async(username) => {
+  const res = await axios.get(`http://localhost:8080/ordercourses/history/${username}`)
+  console.log(res);
+  return res.data;
+}
+
+export const getOrderToolHistory = async(username) => {
+  const res = await axios.get(`http://localhost:8080/orderitems/history/${username}`)
+  console.log(res);
+  return res.data;
+}
 
 export const api = {
   getCourses,
   getMyCourses,
   getCourseById,
   getTools,
+  getCourseDetails,
+  getCourseByCategory,
+  getCourseByInstructor,
   getToolById,
+  getUser,
+  getAllInstructor,
+  getCourseCategory,
+  getTooleCategory,
+  getOrderHistory,
+  getOrderToolHistory,
+  getOrderCourse,
+  getOrderTool,
+  getAllUser, 
+  
   postPayment,
   postComment,
-  getCourseDetails,
+  postProfileImage,
+  postCourse,
+  postCourseImage,
+
   orderCourse,
   orderTool,
-  getCourseByCategory,
-  postProfileImage,
-  getUser,
+
   changePassword,
   changeAccountInfo,
-  getCourseCategory,
-  searchCourse
+
+  searchCourse,
+  searchTool,
+
+  editCourse,
+  editCourseDetail,
+
+  delCourse,
 };
